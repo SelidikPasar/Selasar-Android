9f565cf56f5cf441cc62ae1a90884202da877b5a
fachridantm <fachridantm@gmail.com>
2022-05-29 18:17:32 +0700

    ✨ Config navigation fragment

--- app/src/main/java/com/selasarteam/selidikpasar/view/ui/MainActivity.kt
+++ app/src/main/java/com/selasarteam/selidikpasar/view/ui/MainActivity.kt
@@ -3,6 +3,12 @@ package com.selasarteam.selidikpasar.view.ui
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.selasarteam.selidikpasar.R
 import com.selasarteam.selidikpasar.databinding.ActivityMainBinding
 import com.selasarteam.selidikpasar.view.model.MainViewModel
 import com.selasarteam.selidikpasar.view.model.ViewModelFactory
@@ -17,9 +23,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setupView()
+        setupNavigation()
         setupViewModel()
-        checkUserStatus()
-        setupList()
     }
 
     private fun setupView() {
@@ -27,15 +32,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
     }
 
+    private fun setupNavigation() {
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.nav_news,
+            R.id.nav_price,
+            R.id.nav_marketplace,
+            R.id.nav_profile
+        ).build()
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
     private fun setupViewModel() {
         factory = ViewModelFactory.getInstance(this)
     }
 
-    private fun checkUserStatus() {
-
-    }
-
-    private fun setupList() {
-
-    }
 }
