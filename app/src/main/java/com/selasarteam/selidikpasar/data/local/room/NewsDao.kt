f86256cf404586451f94285694362a65b7c1d5d4
fachridantm <fachridantm@gmail.com>
2022-05-30 20:57:58 +0700

    ✨ Create news list

---
+++ app/src/main/java/com/selasarteam/selidikpasar/data/local/room/NewsDao.kt
@@ -0,0 +1,20 @@
+package com.selasarteam.selidikpasar.data.local.room
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.selasarteam.selidikpasar.data.local.entity.NewsEntity
+
+@Dao
+interface NewsDao {
+    @Query("SELECT * FROM news ORDER BY publishedAt DESC")
+    fun getNews(): LiveData<List<NewsEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertNews(news: List<NewsEntity>)
+
+    @Update
+    suspend fun updateNews(news: NewsEntity)
+
+    @Query("DELETE FROM news")
+    suspend fun deleteAll()
+}
