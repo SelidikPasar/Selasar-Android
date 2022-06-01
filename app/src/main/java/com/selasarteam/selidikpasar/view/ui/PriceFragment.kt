64ccec2839da88a10e983bca062f4522a6aa8b3d
Pradipta Rakahartyanto <67285952+Kirinagi@users.noreply.github.com>
2022-06-01 13:32:26 +0700

    added icons, added fragment lists for detailed information about what items there are(not working yet)

--- app/src/main/java/com/selasarteam/selidikpasar/view/ui/PriceFragment.kt
+++ app/src/main/java/com/selasarteam/selidikpasar/view/ui/PriceFragment.kt
@@ -1,10 +1,12 @@
 package com.selasarteam.selidikpasar.view.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.selasarteam.selidikpasar.R
 import com.selasarteam.selidikpasar.databinding.FragmentPriceBinding
 import com.selasarteam.selidikpasar.view.model.ViewModelFactory
 
@@ -22,12 +24,30 @@ class PriceFragment : Fragment() {
         _binding = FragmentPriceBinding.inflate(inflater, container, false)
 
         setupViewModel()
+        setupMore()
 
         return binding.root
     }
 
     private fun setupViewModel() {
         factory = ViewModelFactory.getInstance(requireContext())
+    }
+
+    private fun setupMore(){
+//        val priceFragment = PriceFragment()
+        val itemFragment = ItemFragment()
+
+
+        binding.ibMore.setOnClickListener{
+//            val intent = Intent(activity, ItemFragment::class.java)
+//            activity?.startActivity(intent)
+
+            childFragmentManager.beginTransaction().apply {
+                replace(R.id.price_fragment, itemFragment)
+                commit()
+            }
+
+        }
     }
 
     override fun onDestroyView() {
