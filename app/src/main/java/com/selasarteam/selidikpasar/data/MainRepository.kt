85986718b61ff93c46ddaf98809d045fd1c8d6e5
fachridantm <fachridantm@gmail.com>
2022-06-01 15:05:51 +0700

    ✨ Create function for user session

--- app/src/main/java/com/selasarteam/selidikpasar/data/MainRepository.kt
+++ app/src/main/java/com/selasarteam/selidikpasar/data/MainRepository.kt
@@ -2,9 +2,11 @@ package com.selasarteam.selidikpasar.data
 
 import android.util.Log
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.liveData
 import androidx.lifecycle.map
 import com.selasarteam.selidikpasar.BuildConfig
+import com.selasarteam.selidikpasar.data.local.datastore.SessionModel
 import com.selasarteam.selidikpasar.data.local.entity.NewsEntity
 import com.selasarteam.selidikpasar.data.local.room.NewsDao
 import com.selasarteam.selidikpasar.data.remote.service.ApiService
@@ -40,6 +42,22 @@ class MainRepository private constructor(
         val localData: LiveData<Result<List<NewsEntity>>> =
             newsDao.getNews().map { Result.Success(it) }
         emitSource(localData)
+    }
+
+    fun getSession(): LiveData<SessionModel> {
+        return preferences.getSession().asLiveData()
+    }
+
+    suspend fun saveSession(session: SessionModel) {
+        preferences.saveSession(session)
+    }
+
+    suspend fun login() {
+        preferences.login()
+    }
+
+    suspend fun logout() {
+        preferences.logout()
     }
 
     companion object {
