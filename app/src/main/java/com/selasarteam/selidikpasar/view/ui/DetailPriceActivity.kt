5ffdd34b94474c82ca739c290571d171c20d0d1e
fachridantm <fachridantm@gmail.com>
2022-06-05 16:34:42 +0700

    ✨ Cleaning merge

--- app/src/main/java/com/selasarteam/selidikpasar/view/ui/DetailPriceActivity.kt
+++ app/src/main/java/com/selasarteam/selidikpasar/view/ui/DetailPriceActivity.kt
@@ -3,12 +3,19 @@ package com.selasarteam.selidikpasar.view.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.selasarteam.selidikpasar.R
+import com.selasarteam.selidikpasar.databinding.ActivityDetailPriceBinding
 
 class DetailPriceActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailPriceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_price)
+        setupView()
+    }
 
-        supportActionBar?.hide()
+    private fun setupView() {
+        binding = ActivityDetailPriceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
