19deff8a3ce35701351f1503ba52d44ba7cced7e
fachridantm <fachridantm@gmail.com>
2022-06-05 13:27:18 +0700

    ✨ Create Google Maps

--- app/src/main/java/com/selasarteam/selidikpasar/view/ui/MarketplaceFragment.kt
+++ app/src/main/java/com/selasarteam/selidikpasar/view/ui/MarketplaceFragment.kt
@@ -5,29 +5,73 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.selasarteam.selidikpasar.R
 import com.selasarteam.selidikpasar.databinding.FragmentMarketplaceBinding
 import com.selasarteam.selidikpasar.view.model.ViewModelFactory
+
 
 class MarketplaceFragment : Fragment() {
 
     private var _binding: FragmentMarketplaceBinding? = null
     private val binding get() = _binding!!
-    private lateinit var factory: ViewModelFactory
+
+    private var factory: ViewModelFactory? = null
+    private var gMap: GoogleMap? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentMarketplaceBinding.inflate(inflater, container, false)
-
-        setupViewModel()
-
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViewModel()
+        setupMaps()
+        setupList()
+    }
+
     private fun setupViewModel() {
-        factory = ViewModelFactory.getInstance(requireContext())
+        factory = ViewModelFactory.getInstance(requireActivity())
+    }
+
+    private fun setupMaps() {
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        gMap = googleMap
+        gMap!!.uiSettings.apply {
+            isRotateGesturesEnabled = true
+            isZoomControlsEnabled = true
+            isIndoorLevelPickerEnabled = true
+            isCompassEnabled = true
+            isMapToolbarEnabled = true
+        }
+
+        val bekasi = LatLng(-6.241586, 106.992416)
+        gMap!!.animateCamera(CameraUpdateFactory.newLatLngZoom(bekasi, 15f))
+    }
+
+    private fun setupList() {
+//        viewModel.list.observe(requireActivity()) {
+//            it?.listStory?.forEach { list ->
+//                gMap!!.addMarker(
+//                    MarkerOptions()
+//                        .position(LatLng(list.lat, list.lon))
+//                        .title("Toko : ${list.name}")
+//                        .snippet("ID : ${list.id}")
+//                )
+//            }
+//        }
     }
 
     override fun onDestroyView() {
