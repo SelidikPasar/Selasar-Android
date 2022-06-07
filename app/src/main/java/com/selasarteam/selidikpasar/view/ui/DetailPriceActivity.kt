5ab67bab21e350ca072ff888d8b888e58a6b5a80
Pradipta Rakahartyanto <67285952+Kirinagi@users.noreply.github.com>
2022-06-07 22:16:21 +0700

    dropdown added
    dropdown added, though functionallity is minimal, need the data.

--- app/src/main/java/com/selasarteam/selidikpasar/view/ui/DetailPriceActivity.kt
+++ app/src/main/java/com/selasarteam/selidikpasar/view/ui/DetailPriceActivity.kt
@@ -2,11 +2,23 @@ package com.selasarteam.selidikpasar.view.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.selasarteam.selidikpasar.R
+import com.selasarteam.selidikpasar.databinding.ActivityDetailPriceBinding
+import com.selasarteam.selidikpasar.databinding.ActivityMorePriceBinding
 
 class DetailPriceActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailPriceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_price)
+        binding = ActivityDetailPriceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val ingredients = resources.getStringArray(R.array.Ingredients)
+        val arrayAdapter = ArrayAdapter(applicationContext, R.layout.dropdown_item, ingredients)
+        binding.detailPrice.setAdapter(arrayAdapter)
     }
 }
